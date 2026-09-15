690b6df43a64cac707b3ea6670697ba745ccec38
th3olegsey <imp.lekha@gmail.com>
2026-09-15 23:01:36 +0300

    rep changes

--- Data/Guild/users/user.py
+++ Data/Guild/users/user.py
@@ -3,14 +3,14 @@ class User():
     def __init__(self, id:int):
         self.id = id
 
-    def get_reputation(self):
-        data = pd.read_csv('Data/Guild/users/reputation.csv')
+    def get_reputation(self) -> int:
+        data = pd.read_csv('Data/Guild/Users/reputation.csv')
         target = data.index[data['id'] == self.id].to_list()[0]
         value = data.iloc[target]
         return value['value']
 
     def set_reputation(self, value:int):
-        data = pd.read_csv('Data/Guild/users/reputation.csv')
+        data = pd.read_csv('Data/Guild/Users/reputation.csv')
         target = data.index[data['id'] == self.id].to_list()[0]
         d = [{
             'id':self.id,
