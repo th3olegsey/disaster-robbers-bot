ba7b327cde40c3130cb1732d09f640113d0e85b2
th3olegsey <imp.lekha@gmail.com>
2026-09-15 18:35:59 +0300

    user class

--- data/guild/users/user.py
+++ data/guild/users/user.py
@@ -1,4 +1,12 @@
+import pandas as pd
 class User():
-    def __init__(self, id:int, reputation:int):
+    def __init__(self, id:int):
         self.id = id
-        self.reputation = reputation
+
+    def reputation(self, method, ammout):
+        if method == 'get':
+            ...
+        if method == 'add':
+            ...
+        if method == 'set':
+            ...
