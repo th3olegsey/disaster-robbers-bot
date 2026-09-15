d222734a4537a66c3e1c7a60bdd0c4b16e54e694
th3olegsey <imp.lekha@gmail.com>
2026-09-15 22:36:13 +0300

    rep changes

--- Commands/reputationCmds.py
+++ Commands/reputationCmds.py
@@ -12,13 +12,16 @@ class ReputationCmds(commands.Cog):
     async def getrep(self, interaction:discord.Interaction, user:discord.User):
         userid = user.id
         userData = User(userid)
-        await interaction.response.send_message(f'{userData.get_reputation()}')
+        await interaction.response.send_message(f'{user.name}: {userData.get_reputation()}')
 
     @app_commands.command(name='setreputation', description='Gets users reputation')
     async def setrep(self, interaction:discord.Interaction, user:discord.User, value:int):
         userid = user.id
         userData = User(userid)
-        await interaction.response.send_message(f'{userData.set_reputation(value)}')
+        old_rep = userData.get_reputation()
+        userData.set_reputation(value)
+        new_rep = value
+        await interaction.response.send_message(f'{user.name}: {old_rep} -> {new_rep}')
 
 async def setup(bot:commands.Bot):
     await bot.add_cog(ReputationCmds(bot))
